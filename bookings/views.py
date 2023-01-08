0688890e87dd5f60228d997543d7ce9080c19b92
leah-jala <leahlafond@protonmail.com>
2023-01-08 23:37:12 +0000

    Add home and reservation links to base.html

--- bookings/views.py
+++ bookings/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.views.generic import DeleteView, CreateView, UpdateView, ListView
 from .models import Reservation, Table
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 from .forms import ReservationForm
 
 def get_booking_page(request):
@@ -21,3 +23,17 @@ def save_form(request):
         form = ReservationForm()
         return render(request, 'bookings/bookings.html', {'form': form})
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('guest_name', css_class='form-group col-md-6 mb-0'),
+                Column('phone', css_class='form-group col-md-6 mb-0'),
+            ),
+            'reservation_date',
+            'reservation_time',
+            'number_guests',
+            'special_requests',
+            Submit('submit', 'Submit', css_class='btn btn-primary')
+        )
