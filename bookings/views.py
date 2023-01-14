39b2e41d2da44e1fd544633f812317b4c2746079
leah-jala <leahlafond@protonmail.com>
2023-01-14 08:35:10 +0000

    Fix: capitalize POST in template, and pk to booked table instance

--- bookings/views.py
+++ bookings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.views.generic import  CreateView, UpdateView, DeleteView, ListView
+from django.contrib import messages
 from .models import Reservation, Table
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
@@ -14,13 +15,13 @@ class CreateReservationView(CreateView):
     success_url = reverse_lazy('homepage')
 
     def form_valid(self, form):
+        form.instance.user = self.request.user
         form.instance.guest_name = form.cleaned_data['guest_name']
         form.instance.phone = form.cleaned_data['phone']
         form.instance.reservation_date = form.cleaned_data['reservation_date']
         form.instance.reservation_time = form.cleaned_data['reservation_time']
         form.instance.number_guests = form.cleaned_data['number_guests']
         form.instance.special_requests = form.cleaned_data['special_requests']
-        form.instance.booked_table_id = Table.objects.first()  # temporarily get tables from database
-        messages.success(self.request, "Your reservation is confirmed!")
+        form.instance.booked_table_id = Table.objects.first().pk  # temporarily get tables from database
 
         return super(CreateReservationView, self).form_valid(form)
