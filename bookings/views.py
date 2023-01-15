543652f19655f9b6980bfac0cb920a2f384c3773
leah-jala <leahlafond@protonmail.com>
2023-01-15 17:40:01 +0000

    Add success message after user posts a booking

--- bookings/views.py
+++ bookings/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import  CreateView, UpdateView, DeleteView, ListView
-from django.contrib import messages
 from .models import Reservation, Table
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
 from .forms import ReservationForm
 
 
-class CreateReservationView(CreateView):
+class CreateReservationView(SuccessMessageMixin, CreateView):
     template_name = 'bookings/bookings.html'
     form_class = ReservationForm
     success_url = reverse_lazy('homepage')
@@ -23,5 +23,9 @@ class CreateReservationView(CreateView):
         form.instance.number_guests = form.cleaned_data['number_guests']
         form.instance.special_requests = form.cleaned_data['special_requests']
         form.instance.booked_table_id = Table.objects.first().pk  # temporarily get tables from database
-
+        self.object = form.save()
         return super(CreateReservationView, self).form_valid(form)
+    
+    def get_success_message(self, cleaned_data):
+        print(cleaned_data)
+        return "Your reservation was successfully submitted."
