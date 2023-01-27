800bc8bb7c5e96628bcdec93d2c84e2895c96504
leah-jala <leahlafond@protonmail.com>
2023-01-27 22:12:13 +0000

    Revert to previous CSS in order to troubleshoot

--- avecplaisir/settings.py
+++ avecplaisir/settings.py
@@ -132,7 +132,6 @@ else:
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
